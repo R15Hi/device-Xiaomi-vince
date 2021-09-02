d2b79d3a484c6085fa1d92a5ed24d76f57f9ead2
Hyper <HyperServer@gmail.com>
2021-09-02 09:09:12 +0530

    Nusantara bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_vince.mk
+    $(LOCAL_DIR)/nad_vince.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_vince-user \
-    lineage_vince-userdebug \
-    lineage_vince-eng
+    nad_vince-user \
+    nad_vince-userdebug \
+    nad_vince-eng
